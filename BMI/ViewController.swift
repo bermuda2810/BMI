f1788e2fa846a08efe53be2643756889355e2c09
vietbq <vietbq@nal.vn>
2020-03-19 10:46:50 +0700

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Bui Quoc Viet on 3/19/20.
+//  Copyright © 2020 ClassiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
